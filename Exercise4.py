13c6ad45834effc410679a52a77c0f521dc17eef
randeephanspal <randeep.sn@gmail.com>
2019-10-01 22:37:34 -0500

    minor changes

--- Exercise4.py
+++ Exercise4.py
@@ -1,7 +1,5 @@
-# Exercise #4: Aircraft Object Initialization
-
 class Aircraft:
-    
+
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
@@ -22,34 +20,41 @@ class Aircraft:
         print("Moved Down..")
         self.y = self.y-1
 
-print("# Exercise 4")
 
-instances=["instance1", "instance2", "instance3", "instance4", "instance5"]
-for i in range(len(instances)):
-    if i==0: instances[i] = Aircraft(0,0)
-    if i==1: instances[i] = Aircraft(3,6)
-    if i==2: instances[i] = Aircraft(6,12)
-    if i==3: instances[i] = Aircraft(9,18)
-    if i==4: instances[i] = Aircraft(12,24)
+if __name__=='__main__':
 
-    print("Creating New Aircraft Object:",i)
-    print("New Aircraft Object Has Just Been Initalized:",i)
-    print("Initial X-Coord:",instances[i].x)
-    print("Initial Y-Coord:",instances[i].y)
+    instances=["instance1", "instance2", "instance3", "instance4", "instance5"]
 
-    instances[i].move_right()
-    instances[i].move_right()
-    instances[i].move_up()
-    instances[i].move_right()
-    instances[i].move_left()
-    instances[i].move_up()
-    instances[i].move_up()
-    instances[i].move_down()
-    instances[i].move_up()
-    instances[i].move_down()
-    instances[i].move_up()
+    def directions():
+        instances[i].move_right()
+        instances[i].move_right()
+        instances[i].move_up()
+        instances[i].move_right()
+        instances[i].move_left()
+        instances[i].move_up()
+        instances[i].move_up()
+        instances[i].move_down()
+        instances[i].move_up()
+        instances[i].move_down()
+        instances[i].move_up()
+    def final_x_y_coord():
+        for i in range(len(instances)):
+            print("\nAircraft [",i,"]")
+            print("Final X-Coord:", instances[i].x)
+            print("Final Y-Coord:", instances[i].y)
 
-for i in range(len(instances)):
-    print("\nAircraft [",i,"]")
-    print("Final X-Coord:", instances[i].x)
-    print("Final Y-Coord:", instances[i].y)
+    for i in range(len(instances)):
+        if i==0: instances[i] = Aircraft(0,0)
+        if i==1: instances[i] = Aircraft(3,6)
+        if i==2: instances[i] = Aircraft(6,12)
+        if i==3: instances[i] = Aircraft(9,18)
+        if i==4: instances[i] = Aircraft(12,24)
+
+        print("Creating New Aircraft Object:",i)
+        print("New Aircraft Object Has Just Been Initalized:",i)
+        print("Initial X-Coord:",instances[i].x)
+        print("Initial Y-Coord:",instances[i].y)
+        directions()
+
+    final_x_y_coord()
+
